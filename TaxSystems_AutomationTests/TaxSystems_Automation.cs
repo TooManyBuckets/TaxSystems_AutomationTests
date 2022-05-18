5e5e834cdd6beaa309ffa4638469b5202f132154
Charles Walker <walker.atlas@yahoo.com>
2022-05-18 23:26:46 +0100

    Updated with correct case names and further NUnit tasks

--- TaxSystems_AutomationTests/TaxSystems_Automation.cs
+++ TaxSystems_AutomationTests/TaxSystems_Automation.cs
@@ -13,17 +13,19 @@ namespace TaxSystemsTestCases
 
         IWebDriver driver;
         IWebElement element;
+    
 
-        [SetUp]
+        [OneTimeSetUp]
         public void startBrowser()
         {
             driver = new ChromeDriver("C:\\Users\\walke\\source\\repos\\TooManyBuckets\\TaxSystems_AutomationTests");
             driver.Manage().Window.Maximize();
             driver.Url = "http://automationpractice.com/index.php";
             Console.WriteLine("This is currently functional");
+
         }
 
-  
+
         [Test]
         public void TestCase1()
         {
@@ -33,13 +35,18 @@ namespace TaxSystemsTestCases
 
             //Find the Printed Summer Dress and Select it
             IWebElement DressDisplayed = driver.FindElement(By.LinkText("Printed Summer Dress"));
-            Boolean status = DressDisplayed.Displayed;
-            if (status == false)
+            Boolean ConfirmedDisplayed = DressDisplayed.Displayed;
+            if (ConfirmedDisplayed == false)
             {
                 Assert.Fail();
             }
             driver.FindElement(By.LinkText("Printed Summer Dress")).Click();
 
+        }
+
+        [Test]
+        public void TestCase2()
+        {
             //Adjust the colour of the dress to blue
             driver.FindElement(By.Id("color_14")).Click();
 
@@ -56,10 +63,37 @@ namespace TaxSystemsTestCases
             //Add the dress to the cart
             driver.FindElement(By.CssSelector(".exclusive > span")).Click();
 
+
+            
+        }
+
+        [Test]
+        public void TestCase3()
+        {
             //This wait allows for the checkout option to be displayed
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            Func<IWebDriver, bool> waitforelement = new Func<IWebDriver, bool>((IWebDriver Web) =>
+                {
+                    Console.WriteLine("Confirmed Working");
+                    IWebElement TotalValueCheck = Web.FindElement(By.XPath("//span[contains(@class,'ajax_block_cart_total')]"));
+                    if(TotalValueCheck != null)
+                    {
+                        return true;
+                    }
+                    return false;
+                });
+            //wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            wait.Until(waitforelement);
 
             //This selects the checkout button and confirms the next page
+            IWebElement TotalValueCheck = driver.FindElement(By.XPath(string.Format("//span[contains(@class,'ajax_block_cart_total')]")));
+            String PopupDisplayed = TotalValueCheck.Text;
+            Console.Write(PopupDisplayed);
+            if (PopupDisplayed != "59.96")
+            {
+                Console.Write("The total amount for this = " + PopupDisplayed + ". This is not the correct total");
+                Assert.Fail();
+            }
             driver.FindElement(By.CssSelector(".button-medium > span")).Click();
             driver.FindElement(By.Name("quantity_5_24_0_0")).Click();
             {
@@ -67,10 +101,19 @@ namespace TaxSystemsTestCases
             }
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
+        }
+
+        [Test]
+        public void TestCase4()
+        {
             //Update the amount to 3 dresses
             driver.FindElement(By.CssSelector(".icon-plus")).Click();
             driver.FindElement(By.CssSelector("#product_5_24_0_0 > .cart_total")).Click();
 
+        }
+        [Test]
+        public void TestCase5()
+        { 
             //Validate that correct values are appearing on the screen
             driver.FindElement(By.XPath("//*[contains(text(),'Printed Summer Dress')]"));
             driver.FindElement(By.XPath("//*[contains(text(),'Color : Blue')]"));
@@ -86,7 +129,7 @@ namespace TaxSystemsTestCases
         }
       
 
-        [TearDown]
+        [OneTimeTearDown]
         public void CloseBrowser()
         {
             driver.Close();
