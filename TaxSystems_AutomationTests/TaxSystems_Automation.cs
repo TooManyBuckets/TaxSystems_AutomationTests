c0637a7d7795bef6a55cda6fe0067946e975da2b
Charles Walker <walker.atlas@yahoo.com>
2022-05-22 21:24:52 +0100

    Fully Passed

--- TaxSystems_AutomationTests/TaxSystems_Automation.cs
+++ TaxSystems_AutomationTests/TaxSystems_Automation.cs
@@ -1,7 +1,6 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 
 namespace TaxSystemsTestCases
@@ -25,7 +24,7 @@ namespace TaxSystemsTestCases
 
 
         [Test]
-        public void TestCase1()
+        public void TestCase01()
         {
             //Search for the general title of the dress
             driver.FindElement(By.Id("search_query_top")).SendKeys("Printed Summer Dress");
@@ -43,7 +42,7 @@ namespace TaxSystemsTestCases
         }
 
         [Test]
-        public void TestCase2()
+        public void TestCase02()
         {
             //Adjust the colour of the dress to blue
             driver.FindElement(By.Id("color_14")).Click();
@@ -60,42 +59,26 @@ namespace TaxSystemsTestCases
 
             //Add the dress to the cart
             driver.FindElement(By.CssSelector(".exclusive > span")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-
-
+            //This wait allows for the checkout option to be displayed
+            Thread.Sleep(10000);
         }
 
-        [Test]
-        public void TestCase3()
-        {
-            //This wait allows for the checkout option to be displayed
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            Func<IWebDriver, bool> waitforelement = new Func<IWebDriver, bool>((IWebDriver Web) =>
-                {
-                    IWebElement TotalValueCheck = Web.FindElement(By.XPath("//span[contains(@class,'ajax_block_cart_total')]"));
-                    if (TotalValueCheck != null)
-                    {
-                        return true;
-                    }
-                    return false;
-                });
-            //wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-            wait.Until(waitforelement);
-            /*
-                        //This selects the checkout button and confirms the next page
-                        IWebElement TotalValueCheck = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[3]/span"));
-                        Console.WriteLine(TotalValueCheck);
 
-                        String PopupDisplayed = TotalValueCheck.Text;
-                        //String PopupDisplayed = driver.FindElement(By.XPath(String.Format("//*[contains(@class,'ajax_block_cart_total')]"))).GetAttribute("value");
-                        Console.Write(PopupDisplayed);
-                        if (PopupDisplayed != "$59.96")
-                        {
-                            Console.Write("The total amount for this = " + PopupDisplayed + ". This is not the correct total");
-                            Assert.Fail();
-                        }
-                        */
+        [Test]
+        public void TestCase03()
+        {
+            //This selects the checkout button and confirms the next pa
+            IWebElement PopupTotal = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[1]/div[2]/div[2]/span"));
+            String PopupTotalText = PopupTotal.Text;
+            Console.Write(PopupTotalText);
+            if (PopupTotalText != "$57.96")
+            {
+                Assert.Fail("The total amount for this = " + PopupTotalText + ". This is not the correct total");
+            }
+
+            //This continues to the checkout page
             driver.FindElement(By.CssSelector(".button-medium > span")).Click();
             driver.FindElement(By.Name("quantity_5_24_0_0")).Click();
             {
@@ -106,27 +89,31 @@ namespace TaxSystemsTestCases
         }
 
         [Test]
-        public void TestCase4()
-        {
-            //Update the amount to 3 dresses
-            driver.FindElement(By.CssSelector(".icon-plus")).Click();
-            driver.FindElement(By.CssSelector("#product_5_24_0_0 > .cart_total")).Click();
-
-        }
-        [Test]
-        public void TestCase5()
+        public void TestCase04()
         {
             //Validate that the full total after tax is correct
-            IWebElement ConfirmTotal = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[6]/td[2]/span"));
+            IWebElement ConfirmDress = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a"));
+            String ConfirmTotalText = ConfirmDress.Text;
+            if (ConfirmTotalText != "Printed Summer Dress")
+            {
+                Assert.Fail("The '" + ConfirmTotalText + "' is not the correct product.");
+            }
+        }
+
+        [Test]
+        public void TestCase05()
+        {
+            //Validate that the full total after tax is correct
+            IWebElement ConfirmTotal = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[7]/td[2]/span"));
             String ConfirmTotalText = ConfirmTotal.Text;
-            if (ConfirmTotalText != "$88.94")
+            if (ConfirmTotalText != "$59.96")
             {
                 Assert.Fail("The full total '" + ConfirmTotalText + "' is incorrect.");
             }
         }
 
         [Test]
-        public void TestCase6()
+        public void TestCase06()
         {
             //Validate that the size and colour are correct
             IWebElement ConfirmConfig = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/small[2]/a"));
@@ -138,19 +125,19 @@ namespace TaxSystemsTestCases
         }
 
         [Test]
-        public void TestCase7()
+        public void TestCase07()
         {
             //Validate that the total cost of products is correct
             IWebElement ConfirmSubTotal = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[1]/td[3]"));
             String ConfirmSubTotalText = ConfirmSubTotal.Text;
-            if (ConfirmSubTotalText != "$86.94")
+            if (ConfirmSubTotalText != "$57.96")
             {
                 Assert.Fail("The subtotal of products '" + ConfirmSubTotalText + "' is incorrect.");
             }
         }
 
         [Test]
-        public void TestCase8()
+        public void TestCase08()
         {
             //Validate that the total cost of products is correct
             IWebElement ConfirmShipping = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[3]/td[2]"));
@@ -162,8 +149,80 @@ namespace TaxSystemsTestCases
         }
 
         [Test]
-        public void TestCase9()
-        { 
+        public void TestCase09()
+        {
+            //Update the amount to 3 dresses
+            driver.FindElement(By.CssSelector(".icon-plus")).Click();
+            driver.FindElement(By.CssSelector("#product_5_24_0_0 > .cart_total")).Click();
+
+        }
+
+        /// <summary>
+        /// ////////////////////////////////////////////////////////////////////////////
+        /// </summary>
+
+        [Test]
+        public void TestCase10()
+        {
+            //Validate that the full total after tax is correct
+            IWebElement ConfirmDress = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a"));
+            String ConfirmTotalText = ConfirmDress.Text;
+            if (ConfirmTotalText != "Printed Summer Dress")
+            {
+                Assert.Fail("The '" + ConfirmTotalText + "' is not the correct product.");
+            }
+        }
+
+        [Test]
+        public void TestCase11()
+        {
+            //Validate that the full total after tax is correct
+            IWebElement ConfirmTotal = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[6]/td[2]/span"));
+            String ConfirmTotalText = ConfirmTotal.Text;
+            if (ConfirmTotalText != "$88.94")
+            {
+                Assert.Fail("The full total '" + ConfirmTotalText + "' is incorrect.");
+            }
+        }
+
+        [Test]
+        public void TestCase12()
+        {
+            //Validate that the size and colour are correct
+            IWebElement ConfirmConfig = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/small[2]/a"));
+            String ConfirmConfigText = ConfirmConfig.Text;
+            if (ConfirmConfigText != "Color : Blue, Size : M")
+            {
+                Assert.Fail("The size and colour of the dress being '" + ConfirmConfigText + "' is incorrect.");
+            }
+        }
+
+        [Test]
+        public void TestCase13()
+        {
+            //Validate that the total cost of products is correct
+            IWebElement ConfirmSubTotal = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[1]/td[3]"));
+            String ConfirmSubTotalText = ConfirmSubTotal.Text;
+            if (ConfirmSubTotalText != "$86.94")
+            {
+                Assert.Fail("The subtotal of products '" + ConfirmSubTotalText + "' is incorrect.");
+            }
+        }
+
+        [Test]
+        public void TestCase14()
+        {
+            //Validate that the total cost of products is correct
+            IWebElement ConfirmShipping = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tfoot/tr[3]/td[2]"));
+            String ConfirmShippingText = ConfirmShipping.Text;
+            if (ConfirmShippingText != "$2.00")
+            {
+                Assert.Fail("The total cost of shipping '" + ConfirmShippingText + "' is incorrect.");
+            }
+        }
+        [Test]
+        public void TestCase15()
+        {
             //Navigate to final checkout page
             driver.FindElement(By.CssSelector(".standard-checkout > span")).Click();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
